e1b5e61ea3c9d9a20d17c2f32edcb995b03d58aa
Ryan <morenoryan33@gmail.com>
2023-01-04 04:27:05 -0500

    first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = require("./app");
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+});
+
+module.exports = app;
